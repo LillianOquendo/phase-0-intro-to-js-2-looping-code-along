8cbc807871e5ce2203adb808791076afea1ca22c
Lillian Oquendo <LillianOquendo1@gmail.com>
2023-03-14 00:31:28 -0400

    creates myfile.md

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+
+function writeCards(names, event){
+const container = []
+    for (let i = 0; i < names.length; i++){
+        container.push (`Thank you, ${names [i]}, for the wonderful ${event} gift!`);
+
+}
+return container;
+}
+console.log(writeCards (names, "surprise"));
+
+
+function countDown (i){
+    while (i >= 0){
+    console.log (i);
+    i--;
+}
+}
+countDown(10);
